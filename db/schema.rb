87ae04f00639ab231e75ccc6f17edafeb23d8e12
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-01-14 18:14:13 +0200

    rollback db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_224015) do
-
-  create_table "events", force: :cascade do |t|
-    t.datetime "date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "creator"
-  end
+ActiveRecord::Schema.define(version: 2020_01_13_223621) do
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
